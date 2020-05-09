782aa9e447dea9da02a0de8c0a1cd67cbade5f7d
Sophal Horn <horn.sophal@gmail.com>
2020-05-08 17:15:06 -0700

    adjust CSS.

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,20 @@ import React from 'react';
 function Card({suit, number}) {
   return (
     <div className="card">
-      {`suit: ${suit}, number: ${number}`}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 100 140" >
+        <text
+          x="50%"
+          y="40px"
+          dominant-baseline="middle"
+          text-anchor="middle">{number}</text>
+        <text
+          x="50%"
+          y="100px"
+          dominant-baseline="middle"
+          text-anchor="middle">{suit}</text>
+      </svg>
     </div>
   );
 }
